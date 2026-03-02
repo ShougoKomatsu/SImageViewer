60c560c648513ab132ac36bc72a369f926bd8c64
Shogo Komatsu <49989073+ShougoKomatsu@users.noreply.github.com>
2026-03-02 22:06:39 +0900

    ズーム機能地下分をマージしてビルドした

--- ImageProc.h
+++ ImageProc.h
@@ -5,3 +5,6 @@
 	BOOL ConvertImage(ImgRGB* imgRGB, CImage* cimage);	
 
 	BOOL ConvertImage(CImage* cimage, ImgRGB* imgRGB);
+bool CreateZoomedImage(CImage* imgOriginal, CImage* imgZoomed, const int iZoomFactor,  const int iCenterR, const int iCenterC);
+//bool CopyImage(CImage* imgSrc, CImage* imgDst);
+BOOL ZoomImage(CImage* imgSrc, CImage* imgDst, const double iR0_Src, const double iC0_Src, const double dScale, const int iWidth_Dst, const int iHeight_Dst);
